7118121620deca1f5891cee1356ee8b0c5075251
andika-big <74223810+andika-big@users.noreply.github.com>
2020-12-30 13:03:29 +0700

    Add files via upload

---
+++ DateTimeBig/src/DateTime/ExampleSplitCalendar.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DateTime;
+
+import java.util.Calendar;
+
+public class ExampleSplitCalendar {
+     public static void main(String[] args) {
+       
+        Calendar calendar = Calendar.getInstance();
+        System.out.println("Waktu sekarang adalah " + calendar.getTime());
+        
+        System.out.println("Tanggal : " + calendar.get(Calendar.DATE));
+        System.out.println("Bulan   : " + calendar.get(Calendar.MONTH));
+        System.out.println("Tahun   : " + calendar.get(Calendar.YEAR));
+    }
+}
